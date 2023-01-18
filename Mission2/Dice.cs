c006da585a3c40364f468330884e96e169714d73
kevin <kevin@KEVIN_LAPTOP>
2023-01-18 15:32:27 -0700

    Add project files.

---
+++ Mission2/Dice.cs
@@ -0,0 +1,99 @@
+﻿// Kevin Paul
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Dice
+    {
+        private int[] rollValues = new int[11];
+
+        //Rolls two dice
+        private int Roll()
+        {
+            Random rnd = new Random();
+
+            int roll1 = rnd.Next(1,7);
+            int roll2 = rnd.Next(1,7);
+
+            return (roll1 + roll2);
+        }
+
+        //Rolls dice for specified amount and stores values in array
+        public void RollDice(int rollAmount)
+        {
+            for (int i = 0; i < rollAmount; i++)
+            {
+                int rollValue = Roll();
+
+                if (rollValue == 2)
+                {
+                    rollValues[0] += 1;
+                }
+                else if (rollValue == 3)
+                {
+                    rollValues[1] += 1;
+                }
+                else if (rollValue == 4)
+                {
+                    rollValues[2] += 1;
+                }
+                else if (rollValue == 5)
+                {
+                    rollValues[3] += 1;
+                }
+                else if (rollValue == 6)
+                {
+                    rollValues[4] += 1;
+                }
+                else if (rollValue == 7)
+                {
+                    rollValues[5] += 1;
+                }
+                else if (rollValue == 8)
+                {
+                    rollValues[6] += 1;
+                }
+                else if (rollValue == 9)
+                {
+                    rollValues[7] += 1;
+                }
+                else if (rollValue == 10)
+                {
+                    rollValues[8] += 1;
+                }
+                else if (rollValue == 11)
+                {
+                    rollValues[9] += 1;
+                }
+                else
+                {
+                    rollValues[10] += 1;
+                }
+            }
+        }
+
+        //Displays all the stats
+        public void DisplayStats()
+        {
+            int numRolls = rollValues.Sum();
+
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach \" * \" represents 1 % of the total number of rolls.\nTotal number of rolls = " + numRolls + ".\n");
+            for (int i = 0; i < 11; i++)
+            {
+                int asteriskAmount = (int)((rollValues[i] / (decimal)numRolls) * 100);
+
+                Console.Write("\n" + (i + 2) + ": ");
+                
+                for (int j = 0; j < asteriskAmount; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
---
+++ Mission2/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+Kevin Paul
+
+Write a program that simulates the rolling of two 6-sided dice. Use an Array to keep track of the
+number of times that each total number is thrown. In other words, keep track of how many times
+the combination of the two simulated dice is 2, how many times the combination is 3, and so on,
+all the way up through 12.
+
+Allow the user to choose how many times the “dice” will be thrown. Then, once the dice have
+been thrown the specified number of times, print a histogram (using the* character) that shows
+the total percentage each number was rolled. Each * will represent 1% of the total rolls.
+*/
+
+using System;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int totalRolls = Convert.ToInt32(Console.ReadLine());
+
+            Dice dice = new Dice();
+
+            // Simulates rolling 2 dice for specified amount
+            dice.RollDice(totalRolls);
+
+            dice.DisplayStats();
+
+            Console.WriteLine("\n\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
